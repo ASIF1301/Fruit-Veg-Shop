ac78b3b866deb7a7bd3335d6f3e5ee55c7e531c9
ASIF1301 <asifrock003@gmail.com>
2026-04-18 10:48:25 +0530

    Delete backend/routes directory

--- backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const products = require("../data/products.json");
-
-router.get("/", (req, res) => {
-  res.json(products);
-});
-
-module.exports = router;
